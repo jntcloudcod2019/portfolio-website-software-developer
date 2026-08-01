b65822cc6c3d20b33cb1c897e38cee023cdd1215
Jonathan Fernando <Jonathanfrnnd3@gmail.com>
2026-08-01 18:58:31 -0300

    Atualizacao de cv

--- api/_lib/prisma.ts
+++ api/_lib/prisma.ts
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion, Db } from 'mongodb';
 
-const DATABASE_URL = 'mongodb+srv://j1997silva_db_user:C9cqCcYxgL1jx6R2@cluster0.c7myaex.mongodb.net/?appName=Cluster0';
+const DATABASE_URL = "mongodb+srv://j1997silva_db_user:C9cqCcYxgL1jx6R2@cluster0.c7myaex.mongodb.net/?appName=Cluster0";
 
 const globalForMongo = globalThis as unknown as { client: MongoClient | undefined };
 
